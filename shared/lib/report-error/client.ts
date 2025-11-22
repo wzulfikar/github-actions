fc4ea5c6241263bc1c822f4741602a2c4cfdc2ca
Github Action <>
2025-11-23 00:36:12 +0200

    add sentry

--- shared/lib/report-error/client.ts
+++ shared/lib/report-error/client.ts
@@ -1,3 +1,18 @@
-export async function reportClientError(e: unknown, ctx?: object) {
-  console.log('client error:', e, ctx)
+import Sentry from '@sentry/browser'
+
+export async function reportClientError(
+  error: unknown,
+  params: Record<string, unknown>,
+) {
+  const { ctx, level, userId, ...rest } = params
+
+  const payload = { ...rest } as Record<string, unknown>
+  if (ctx) payload.extra = { context: ctx }
+  if (level) payload.level = level
+  if (userId) payload.user = { id: userId }
+
+  Sentry.captureException(error, payload)
+  await Sentry.flush().catch((e) => {
+    console.error('[error] reportClientError: `Sentry.flush` failed. error:', e)
+  })
 }
--- shared/lib/report-error/server.ts
+++ shared/lib/report-error/server.ts
@@ -1,3 +1,18 @@
-export async function reportServerError(e: unknown, ctx?: object) {
-  console.log('server error:', e, ctx)
+import Sentry from '@sentry/node'
+
+export async function reportServerError(
+  error: unknown,
+  params: Record<string, unknown>,
+) {
+  const { ctx, level, userId, ...rest } = params
+
+  const payload = { ...rest } as Record<string, unknown>
+  if (ctx) payload.extra = { context: ctx }
+  if (level) payload.level = level
+  if (userId) payload.user = { id: userId }
+
+  Sentry.captureException(error, payload)
+  await Sentry.flush().catch((e) => {
+    console.error('[error] reportServerError: `Sentry.flush` failed. error:', e)
+  })
 }
